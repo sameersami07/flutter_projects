d6ddc57825f3963cd3cea309e88f50e30d73135d
sameersami07 <166928196+sameersami07@users.noreply.github.com>
2025-01-17 14:02:11 +0530

    main.dart

---
+++ application_register/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Firebase Registration',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: RegisterScreen(),
+      routes: {
+        '/home': (context) => HomeScreen(),
+      },
+    );
+  }
+}
